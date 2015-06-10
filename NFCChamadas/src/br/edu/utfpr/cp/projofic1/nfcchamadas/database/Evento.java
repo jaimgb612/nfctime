0520def9126551f5c4add16f0ccb8f4ccf234048
GuDiasOliveira <gust.dias@gmail.com>
2015-06-10 13:02:18 -0300

    com conflitos.

--- NFCChamadas/src/br/edu/utfpr/cp/projofic1/nfcchamadas/database/Evento.java
+++ NFCChamadas/src/br/edu/utfpr/cp/projofic1/nfcchamadas/database/Evento.java
@@ -3,14 +3,11 @@ package br.edu.utfpr.cp.projofic1.nfcchamadas.database;
 import java.util.Calendar;
 
 import android.annotation.SuppressLint;
-<<<<<<< HEAD
-=======
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
-
 
 @SuppressLint("SimpleDateFormat")
 public class Evento {
@@ -18,18 +15,14 @@ public class Evento {
 	public static final String HORA_FORMAT = "HH:mm"; // Baseado em 24 horas
 	public static final String DATA_FORMAT = "dd/MM/yyyy";
 	
-<<<<<<< HEAD
 	private long id, idCriadorEvento;
 	private String nome;
 	private Calendar horaInicio, horaFim, data;
-=======
 	private int id;
 	private String nome;
 	private Date data;
 	private String horaInicio, horaFim, idCriadorEvento;
 	private boolean gravado;
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
-	
 	public int getId() {
 		return id;
 	}
@@ -50,16 +43,16 @@ public class Evento {
 	}
 	
 	
-<<<<<<< HEAD
+
 	public Calendar getData() {
-=======
+
 	public Date getData() {
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
+
 		return data;
 	}
 	
 	
-<<<<<<< HEAD
+
 	public void setData(Calendar data) throws IllegalArgumentException {
 //		// Primeiramente validando o formato da data
 //		try {
@@ -67,11 +60,10 @@ public class Evento {
 //		} catch (ParseException e) {
 //			throw new IllegalArgumentException("Formato de data errado", e);
 //		}
-=======
+
 	public void setData(Date data) throws IllegalArgumentException {
 		// Primeiramente validando o formato da data
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
-		
+
 		this.data = data;
 	}
 	
@@ -80,14 +72,12 @@ public class Evento {
 		return horaInicio;
 	}
 	
-	
-<<<<<<< HEAD
+
 	public void setHoraInicio(Calendar horaInicio) throws IllegalArgumentException {
 //		validateHora(horaInicio);
-=======
+
 	public void setHoraInicio(String horaInicio) throws IllegalArgumentException {
 	
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
 		this.horaInicio = horaInicio;
 	}
 	
@@ -97,13 +87,13 @@ public class Evento {
 	}
 	
 	
-<<<<<<< HEAD
+
 	public void setHoraFim(Calendar horaFim) throws IllegalArgumentException {
 //		validateHora(horaFim);
-=======
+
 	public void setHoraFim(String horaFim) throws IllegalArgumentException {
 		
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
+
 		this.horaFim = horaFim;
 	}
 	
@@ -118,15 +108,6 @@ public class Evento {
 	}
 	
 	
-<<<<<<< HEAD
-//	private void validateHora(String hora) throws IllegalArgumentException {
-//		try {
-//			new SimpleDateFormat(HORA_FORMAT).parse(hora);
-//		} catch (ParseException e) {
-//			throw new IllegalArgumentException("Formato de hora errado", e);
-//		}
-//	}
-=======
 	private void validateHora(String hora) throws IllegalArgumentException {
 		
 	}
@@ -146,5 +127,5 @@ public class Evento {
 	public void setGravado(boolean gravado) {
 		this.gravado = gravado;
 	}
->>>>>>> branch 'master' of https://github.com/joaobracaioli/nfctime
+
 }
